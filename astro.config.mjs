13dbea9ca465ffb9ade2dc46db32ce99c37dea42
sahalkk <msahalkk357@gmail.com>
2026-06-07 17:02:49 +0530

    force astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://halaalworld.com",
+  base: "/",
   vite: {
     plugins: [tailwindcss()]
   }
